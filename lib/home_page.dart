a9d0ef3b2e331a3abbae1ca9af44a41f8f105b38
halilkoseh <kose.halil@std.izu.edu.tr>
2024-03-29 17:00:40 +0300

    ISTANBUL

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -26,7 +26,7 @@ class HomePage extends StatelessWidget {
           Image.asset('assets/codelab.png'),
           const SizedBox(height: 8),
           const IconAndDetail(Icons.calendar_today, 'October 30'),
-          const IconAndDetail(Icons.location_city, 'San Francisco'),
+          const IconAndDetail(Icons.location_city, 'istanbul, Turkey'),
           // Add from here
           Consumer<ApplicationState>(
             builder: (context, appState, _) => AuthFunc(
